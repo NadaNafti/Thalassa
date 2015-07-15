c522e738db13a42533442930e0a4a5e32527a4d6
zied666 <zied.kharraz@gmail.com>
2015-07-15 09:47:07 +0100

    add promo to list

--- src/Front/GeneralBundle/Controller/HotelTunisieController.php
+++ src/Front/GeneralBundle/Controller/HotelTunisieController.php
@@ -33,6 +33,45 @@ class HotelTunisieController extends Controller
 	));
     }
 
+    public function filtreAction($page, $categorie, $chaine, $ville, $name)
+    {
+	$em = $this->getDoctrine()->getManager();
+	$session = $this->getRequest()->getSession();
+	$request = $this->getRequest();
+	$pays = $em->getRepository('BackHotelTunisieBundle:Pays')->findOneBy(array('code' => 'tn'));
+	$villes = $em->getRepository('BackHotelTunisieBundle:Ville')->findBy(array('pays' => $pays), array('libelle' => 'asc'));
+//	$chaines = $em->getRepository('BackHotelTunisieBundle:Chaine')->findBy(array(), array('libelle' => 'asc'));
+	$categories = $em->getRepository('BackHotelTunisieBundle:Categorie')->findBy(array(), array('libelle' => 'asc'));
+	if ($request->isMethod('POST'))
+	{
+	    $villeArray = array();
+	    $categorieArray = array();
+	    $arrays = array();
+	    foreach ($villes as $v)
+	    {
+		if ($request->get('ville_' . $v->getId()))
+		    $villeArray[] = $v->getId();
+	    }
+	    foreach ($categories as $cat)
+	    {
+		if ($request->get('categorie_' . $cat->getId()))
+		    $categorieArray[] = $cat->getId();
+	    }
+	    if (count($villeArray) == 0)
+		$arrays['ville'] = 'all';
+	    else
+		$arrays['ville'] = implode(',', $villeArray);
+	    if (count($categorieArray) == 0)
+		$arrays['categorie'] = 'all';
+	    else
+		$arrays['categorie'] = implode(',', $categorieArray);
+	    $arrays['name'] = urlencode($request->get('motclesSearch'));
+	    $session->set('nuitees', $request->get('nuiteesSearch'));
+	    $session->set('dateDebut', $request->get('dateDebutSearch'));
+	    return $this->redirect($this->generateUrl('front_hoteltunisie_list', $arrays));
+	}
+    }
+
     public function listeAction($page, $categorie, $chaine, $ville, $name)
     {
 	$em = $this->getDoctrine()->getManager();
@@ -40,7 +79,7 @@ class HotelTunisieController extends Controller
 	$request = $this->getRequest();
 	$pays = $em->getRepository('BackHotelTunisieBundle:Pays')->findOneBy(array('code' => 'tn'));
 	$villes = $em->getRepository('BackHotelTunisieBundle:Ville')->findBy(array('pays' => $pays), array('libelle' => 'asc'));
-	$chaines = $em->getRepository('BackHotelTunisieBundle:Chaine')->findBy(array(), array('libelle' => 'asc'));
+//	$chaines = $em->getRepository('BackHotelTunisieBundle:Chaine')->findBy(array(), array('libelle' => 'asc'));
 	$categories = $em->getRepository('BackHotelTunisieBundle:Categorie')->findBy(array(), array('libelle' => 'asc'));
 	$hotels = $em->getRepository('BackHotelTunisieBundle:Hotel')->filtreFrontOfficePlus($categorie, $chaine, $ville, $name);
 	$hotels = $this->removeInvalideHotel($hotels);
@@ -49,9 +88,9 @@ class HotelTunisieController extends Controller
 	return $this->render('FrontGeneralBundle:hoteltunisie/liste:liste.html.twig', array(
 		    'hotels' => $hotels,
 		    'villes' => $villes,
-		    'chaines' => $chaines,
+//		    'chaines' => $chaines,
 		    'categories' => $categories,
-		    'motcle' =>  urldecode($name)
+		    'motcle' => urldecode($name)
 	));
     }
 
