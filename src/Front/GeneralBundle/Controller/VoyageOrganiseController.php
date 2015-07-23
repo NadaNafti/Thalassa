0b0b69c0fa605e83a032da649e593bf4cd181d7d
zied666 <zied.kharraz@gmail.com>
2015-07-23 14:57:15 +0100

    add details voyage

--- src/Front/GeneralBundle/Controller/VoyageOrganiseController.php
+++ src/Front/GeneralBundle/Controller/VoyageOrganiseController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class VoyageOrganiseController extends Controller
 {
+
     public function accueilAction()
     {
 	$em = $this->getDoctrine()->getManager();
@@ -13,6 +14,7 @@ class VoyageOrganiseController extends Controller
 	$request = $this->getRequest();
 	return $this->render('FrontGeneralBundle:voyageorganise:accueil.html.twig');
     }
+
     public function listeAction($page)
     {
 	$em = $this->getDoctrine()->getManager();
@@ -20,7 +22,19 @@ class VoyageOrganiseController extends Controller
 	$request = $this->getRequest();
 	$voyages = $em->getRepository('BackVoyageOrganiseBundle:VoyageOrganise')->filtre();
 	$paginator = $this->get('knp_paginator');
-	$hotels = $paginator->paginate($voyages, $page, 20);
-	return $this->render('FrontGeneralBundle:voyageorganise/liste:liste.html.twig');
+	$voyages = $paginator->paginate($voyages, $page, 20);
+	return $this->render('FrontGeneralBundle:voyageorganise/liste:liste.html.twig', array(
+		    'voyages' => $voyages
+	));
     }
+
+    public function detailsAction($slug)
+    {
+	$em = $this->getDoctrine()->getManager();
+	$voyage = $em->getRepository('BackVoyageOrganiseBundle:VoyageOrganise')->findOneBy(array('slug' => $slug));
+	return $this->render('FrontGeneralBundle:voyageorganise/details:details.html.twig', array(
+		    'voyage' => $voyage
+	));
+    }
+
 }
