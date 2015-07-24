64547ea080bc51c8d960b94cf26ad58ecd23e137
zied666 <zied.kharraz@gmail.com>
2015-07-24 10:46:31 +0100

    add reservation voyage organise

--- src/Front/GeneralBundle/Controller/VoyageOrganiseController.php
+++ src/Front/GeneralBundle/Controller/VoyageOrganiseController.php
@@ -3,6 +3,9 @@
 namespace Front\GeneralBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Back\UserBundle\Form\ClientType;
+use Back\VoyageOrganiseBundle\Form\ReservationType;
+use Back\VoyageOrganiseBundle\Entity\Reservation;
 
 class VoyageOrganiseController extends Controller
 {
@@ -31,9 +34,52 @@ class VoyageOrganiseController extends Controller
     public function detailsAction($slug)
     {
 	$em = $this->getDoctrine()->getManager();
+	$session = $this->getRequest()->getSession();
 	$voyage = $em->getRepository('BackVoyageOrganiseBundle:VoyageOrganise')->findOneBy(array('slug' => $slug));
+	$user = $this->get('security.context')->getToken()->getUser();
+	if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') && !is_null($user->getClient()))
+	    $client = $user->getClient();
+	else
+	    $client = $this->container->get('users')->getPassager();
+	$reservation = new Reservation();
+	$reservation->setVoyage($voyage);
+	$form = $this->createForm(new ReservationType(), $reservation)
+		->remove('commentaire')
+		->remove('voyage')
+		->add("client", new ClientType(), array('data' => $client));
+	$request = $this->getRequest();
+	if ($request->isMethod('POST'))
+	{
+	    $form->submit($request);
+	    $reservation = $form->getData();
+	    if (count($reservation->getAdultes()) == 0 && count($reservation->getEnfants()) == 0)
+	    {
+		$session->getFlashBag()->add('warning', "Vous devez  choisir au moin un adulte ");
+		return $this->redirect($this->generateUrl('front_voyageorganise_details',array('slug'=>$slug)));
+	    }
+	    $reservation->setCoordonnees(array($reservation->getClient()->getNomPrenom(), $reservation->getClient()->getTel1(), $reservation->getClient()->getTel2(), $reservation->getClient()->getAdresse()));
+	    $em->persist($reservation->setFrontOffice(TRUE));
+	    foreach ($reservation->getAdultes() as $adulte)
+		$em->persist($adulte->setReservationA($reservation)->setAge(null));
+	    foreach ($reservation->getEnfants() as $enfant)
+		$em->persist($enfant->setReservationE($reservation));
+	    $em->flush();
+	    $this->container->get('users')->getPassager();
+	    return $this->redirect($this->generateUrl('front_voyageorganise_thankyou',array('slug'=>$slug,'reservation'=>$reservation->getId())));
+	}
 	return $this->render('FrontGeneralBundle:voyageorganise/details:details.html.twig', array(
-		    'voyage' => $voyage
+		    'voyage' => $voyage,
+		    'form' => $form->createView()
+	));
+    }
+    
+    public function successAction($slug, Reservation $reservation )
+    {
+	$em = $this->getDoctrine()->getManager();
+	$session = $this->getRequest()->getSession();
+	$voyage = $em->getRepository('BackVoyageOrganiseBundle:VoyageOrganise')->findOneBy(array('slug' => $slug));
+	return $this->render('FrontGeneralBundle:voyageorganise:success.html.twig', array(
+		    'reservation' => $reservation,
 	));
     }
 
