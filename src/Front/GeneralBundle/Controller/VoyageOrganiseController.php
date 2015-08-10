2a88bc9d05f9f5f74877fd72f92a15c8c00fff03
zied666 <zied.kharraz@gmail.com>
2015-08-10 14:41:50 +0100

    add code reservation

--- src/Front/GeneralBundle/Controller/VoyageOrganiseController.php
+++ src/Front/GeneralBundle/Controller/VoyageOrganiseController.php
@@ -5,11 +5,14 @@ namespace Front\GeneralBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Back\UserBundle\Form\ClientType;
 use Back\VoyageOrganiseBundle\Form\ReservationType;
-use Back\VoyageOrganiseBundle\Entity\Reservation;
 use Back\VoyageOrganiseBundle\Entity\Periode;
 use Back\VoyageOrganiseBundle\Entity\Pack;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Back\VoyageOrganiseBundle\Entity\Reservation;
+use Back\VoyageOrganiseBundle\Entity\ReservationChambre;
+use Back\VoyageOrganiseBundle\Entity\ReservationLigne;
+use Back\VoyageOrganiseBundle\Entity\ReservationPersonne;
 
 class VoyageOrganiseController extends Controller
 {
@@ -144,76 +147,11 @@ class VoyageOrganiseController extends Controller
         return $response;
     }
 
-//    public function reservationAction($slug, Periode $periode)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//        $session = $this->getRequest()->getSession();
-//        $request = $this->getRequest();
-//        $voyage = $em->getRepository('BackVoyageOrganiseBundle:VoyageOrganise')->findOneBy(array('slug' => $slug));
-//        $user = $this->get('security.context')->getToken()->getUser();
-//        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') && !is_null($user->getClient()))
-//            $client = $user->getClient();
-//        else
-//            $client = $this->container->get('users')->getPassager();
-//        $form = $this->createFormBuilder()
-//                ->add("client", new ClientType(), array('data' => $client))
-//                ->add("pack", 'entity', array(
-//            'class' => 'BackVoyageOrganiseBundle:Pack',
-//            'query_builder' => function(EntityRepository $er) use ($periode)
-//            {
-//                return $er->createQueryBuilder('p')
-//                        ->where('p.periode = :id')
-//                        ->setParameter('id', $periode->getId())
-//                        ->orderBy('p.id', 'desc');
-//                ;
-//            },
-//            'required' => true,
-//            'empty_value' => 'Liste des packs disponlible',
-//            'empty_data' => null
-//        ));
-//        $form = $form->getForm();
-//        if ($request->isMethod('POST'))
-//        {
-//            $form->submit($request);
-//            if ($form->isValid())
-//            {
-//                $data = $form->getData();
-//                $reservationVO['voyage'] = $periode->getVoyage()->getId();
-//                $reservationVO['periode'] = $periode->getId();
-//                $reservationVO['client'] = $data['client']->getId();
-//                $reservationVO['coordonnes'] = array($data['client']->getNomPrenom(), $data['client']->getTel1(), $data['client']->getTel2(), $data['client']->getAdresse());
-//                $reservationVO['pack'] = $data['pack']->getId();
-//                $session->set('reservationVO', $reservationVO);
-//                return $this->redirect($this->generateUrl('front_voyageorganise_occupationchambre', array('slug' => $slug)));
-//            }
-//        }
-//        return $this->render('FrontGeneralBundle:voyageorganise:reservation.html.twig', array(
-//                    'csrf_token' => $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate'),
-//                    'form' => $form->createView(),
-//                    'periode' => $periode,
-//        ));
-//    }
-//
-//    public function occChambreAction($slug)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//        $session = $this->getRequest()->getSession();
-//        $request = $this->getRequest();
-//        $reservationVO=$session->get('reservationVO');
-//        $periode=$em->getRepository('BackVoyageOrganiseBundle:Periode')->find($reservationVO['periode']);
-//        $pack=$em->getRepository('BackVoyageOrganiseBundle:Pack')->find($reservationVO['pack']);
-//        $form = $this->createFormBuilder();
-//        $form = $form->getForm();
-//        return $this->render('FrontGeneralBundle:voyageorganise:occChambre.html.twig', array(
-//                    'form' => $form->createView(),
-//                    'periode' => $periode,
-//                    'pack'=>$pack
-//        ));
-//    }
-    
     public function reservationAction($slug, Pack $pack)
     {
         $em = $this->getDoctrine()->getManager();
+        $session = $this->getRequest()->getSession();
+        $request = $this->getRequest();
         $user = $this->get('security.context')->getToken()->getUser();
         if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') && !is_null($user->getClient()))
             $client = $user->getClient();
@@ -222,10 +160,127 @@ class VoyageOrganiseController extends Controller
         $form = $this->createFormBuilder()
                 ->add("client", new ClientType(), array('data' => $client));
         $form = $form->getForm();
+        if ($request->isMethod('POST'))
+        {
+            $periode = $pack->getPeriode();
+            $supplements = array();
+            $circuits = array();
+            $frais = array();
+            $chambres = array(
+                array('single', 1, $pack->getSingleAchat(), $pack->getSingleVente()),
+                array('double', 2, $pack->getDoubleAchat(), $pack->getDoubleVente()),
+                array('triple', 3, $pack->getTripleAchat(), $pack->getTripleVente()),
+                array('quadruple', 4, $pack->getQuadrupleAchat(), $pack->getQuadrupleVente())
+            );
+            $form->submit($request);
+            $data = $form->getData();
+            $coordoonnes = array($client->getNomPrenom(), $client->getTel1(), $client->getTel2(), $client->getAdresse());
+            $nbrPassagers = $request->get('chambresingle') + $request->get('chambredouble') * 2 + $request->get('chambretriple') * 3 + $request->get('chambrequadruple') * 4;
+            if ($nbrPassagers == 0)
+            {
+                $session->getFlashBag()->add('Erreur', "Vous devrez choisir au moin une chambre.");
+                return $this->redirect($this->generateUrl('front_voyageorganise_reservation', array('slug' => $slug, 'pack' => $pack->getId())));
+            }
+            elseif ($periode->getDepartGarantie() && $nbrPassagers < $periode->getMin())
+            {
+                $session->getFlashBag()->add('Erreur', "Vous devrez choisir au moin " . $periode->getMin() . " personnes.");
+                return $this->redirect($this->generateUrl('front_voyageorganise_reservation', array('slug' => $slug, 'pack' => $pack->getId())));
+            }
+            $reservation = new Reservation();
+            $reservation->setClient($client)
+                    ->setCoordonnees($coordoonnes)
+                    ->setEtat(1)
+                    ->setFrontOffice(true)
+                    ->setVoyage($periode->getVoyage());
+            $em->persist($reservation);
+            $x = 0;
+            foreach ($chambres as $ch)
+            {
+                for ($i = 1; $i <= $request->get('chambre' . $ch[0]); $i++)
+                {
+                    $reservationChambre = new ReservationChambre();
+                    $reservationChambre->setReservation($reservation)
+                            ->setType($ch[1]);
+                    $em->persist($reservationChambre);
+                    for ($p = 1; $p <= $ch[1]; $p++)
+                    {
+                        $x++;
+                        $reservationPersonne = new ReservationPersonne();
+                        $reservationPersonne->setChambre($reservationChambre)
+                                ->setNomPrenom($request->get($ch[0] . '_nom_' . $i . '_' . $p))
+                                ->setPassport($request->get($ch[0] . '_passport_' . $i . '_' . $p));
+                        if ($request->get($ch[0] . '_age_' . $i . '_' . $p) != 'adulte')
+                            $reservationPersonne->setAge($request->get($ch[0] . '_age_' . $i . '_' . $p))->setEnfant(true);
+                        else
+                            $reservationPersonne->setAge(null)->setEnfant(false);
+                        $em->persist($reservationPersonne);
+                        $reservationLigne = new ReservationLigne();
+                        $reservationLigne->setPersonne($reservationPersonne)
+                                ->setLibelle('Logement chambre ' . $ch[0])
+                                ->setCode('chambre_' . $ch[0])
+                                ->setAchat($ch[2])
+                                ->setVente($ch[3]);
+                        $em->persist($reservationLigne);
+                        foreach ($pack->getSupplements() as $supp)
+                        {
+                            if ($supp->getObligatoire() || $request->get('supplement' . $supp->getId()))
+                            {
+                                if ($x == 1)
+                                    $supplements[] = $supp->getId();
+                                $reservationLigne = new ReservationLigne();
+                                $reservationLigne->setPersonne($reservationPersonne)
+                                        ->setLibelle($supp->getLibelle())
+                                        ->setCode('Supplement')
+                                        ->setAchat($supp->getAdulteAchat())
+                                        ->setVente($supp->getAdulteVente());
+                                $em->persist($reservationLigne);
+                            }
+                        }
+                        foreach ($periode->getCircuits() as $supp)
+                        {
+                            if ($supp->getObligatoire() || $request->get('circuit' . $supp->getId()))
+                            {
+                                if ($x == 1)
+                                    $circuits[] = $supp->getId();
+                                $reservationLigne = new ReservationLigne();
+                                $reservationLigne->setPersonne($reservationPersonne);
+                                if (!$reservationPersonne->getEnfant())
+                                    $reservationLigne->setAchat($supp->getAdulteAchat())->setVente($supp->getAdulteVente())->setCode('CircuitAdl')->setLibelle($supp->getLibelle() . ' adulte');
+                                else
+                                    $reservationLigne->setAchat($supp->getEnfantAchat())->setVente($supp->getEnfantVente())->setCode('CircuitEnf')->setLibelle($supp->getLibelle() . ' enfant');
+                                $em->persist($reservationLigne);
+                            }
+                        }
+                        foreach ($periode->getFrais() as $supp)
+                        {
+                            if ($supp->getObligatoire() || $request->get('frai' . $supp->getId()))
+                            {
+                                if ($x == 1)
+                                    $frais[] = $supp->getId();
+                                $reservationLigne = new ReservationLigne();
+                                $reservationLigne->setPersonne($reservationPersonne);
+                                if (!$reservationPersonne->getEnfant())
+                                    $reservationLigne->setAchat($supp->getAdulteAchat())->setVente($supp->getAdulteVente())->setCode('FraisDiversAdl')->setLibelle($supp->getLibelle() . ' adulte');
+                                else
+                                    $reservationLigne->setAchat($supp->getEnfantAchat())->setVente($supp->getEnfantVente())->setCode('FraisDiversEnf')->setLibelle($supp->getLibelle() . ' enfant');
+                                $em->persist($reservationLigne);
+                            }
+                        }
+                    }
+                }
+            }
+            $em->flush();
+            $em->persist($reservation->setSupplements($supplements)->setCircuits($circuits)->setFrais($frais));
+            return $this->redirect($this->generateUrl('front_voyageorganise_thankyou', array(
+                                'slug' => $slug,
+                                'reservation' => $reservation->getId()
+            )));
+        }
         return $this->render('FrontGeneralBundle:voyageorganise:reservation.html.twig', array(
                     'form' => $form->createView(),
+                    'csrf_token' => $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate'),
                     'periode' => $pack->getPeriode(),
-                    'pack'=>$pack
+                    'pack' => $pack
         ));
     }
 
