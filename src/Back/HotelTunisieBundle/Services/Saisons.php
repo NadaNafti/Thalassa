38451178c253b225a10b28c1eda8dba2d1cbbb1c
zied666 <zied.kharraz@gmail.com>
2015-09-11 14:05:29 +0100

    add user from new reservation page

--- src/Back/HotelTunisieBundle/Services/Saisons.php
+++ src/Back/HotelTunisieBundle/Services/Saisons.php
@@ -10,16 +10,26 @@ use Back\HotelTunisieBundle\Entity\Hotel;
 class Saisons
 {
 
-    public function __construct(EntityManager $entityManager)
+    public function __construct( EntityManager $entityManager )
     {
-	$this->em = $entityManager;
+        $this->em = $entityManager;
     }
 
-    public function getSaisonByClient(Hotel $hotel, Client $client, $date)
+    public function getSaisonByClient( Hotel $hotel,Client $client,$date )
     {
-	if (is_null($client->getAmicale()) || is_null($client))
-	    return $hotel->getSaisonPromotionByDate($date);
-	return $hotel->getSaisonByClient($date, $client);
+        if( is_null( $client->getAmicale() ) || is_null( $client ) )
+            return $hotel->getSaisonPromotionByDate( $date );
+        return $hotel->getSaisonByClient( $date,$client );
+    }
+
+    public function getValideHotel( $hotels )
+    {
+        $newList = array();
+        foreach( $hotels as $hotel ){
+            if( !is_null( $hotel->getSaisonBase() ) && $hotel->getSaisonBase()->isValid() && $hotel->getEtat() == 1 )
+                $newList[] = $hotel;
+        }
+        return $newList;
     }
 
 }
