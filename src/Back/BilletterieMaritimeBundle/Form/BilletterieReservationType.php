9694b5c20d542e5df8d2c44a1017e1f8f7374546
zied666 <zied.kharraz@gmail.com>
2015-09-22 16:14:01 +0100

    update form billetterie

--- src/Back/BilletterieMaritimeBundle/Form/BilletterieReservationType.php
+++ src/Back/BilletterieMaritimeBundle/Form/BilletterieReservationType.php
@@ -5,6 +5,7 @@ namespace Back\BilletterieMaritimeBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Back\BilletterieMaritimeBundle\Form\BilletterieReservationLigneType;
 
 class BilletterieReservationType extends AbstractType
 {
@@ -16,7 +17,13 @@ class BilletterieReservationType extends AbstractType
     {
         $builder
             ->add('commentaire')
-            ->add('typeVol')
+            ->add('typeVol','choice',array(
+                'choices'=>array(
+                    1=>"Aller-Retour",
+                    2=>"Aller-Simple",
+                    3=>"Multi-Segments",
+                )
+            ))
             ->add('nomPrenom')
             ->add('email')
             ->add('cin')
@@ -24,17 +31,28 @@ class BilletterieReservationType extends AbstractType
             ->add('codePostal')
             ->add('ville')
             ->add('company')
-            ->add('classe')
+            ->add('classe','choice',array(
+                'choices'=>array(
+                    1=>"Economy",
+                    2=>"Business",
+                    3=>"First",
+                )
+            ))
             ->add('lieuDepart')
             ->add('lieuArriver')
-            ->add('dateDepart')
-            ->add('deteArriver')
-            ->add('responsable')
-            ->add('client')
-            ->add('lignes','collection',new BilletterieReservationLigneType())
-        ;
+            ->add('dateDepart', 'date', array(
+                'required' => FALSE,
+                'widget'   => 'single_text',
+                'format'   => 'yyyy-MM-dd',
+            ))
+            ->add('deteArriver', 'date', array(
+                'required' => FALSE,
+                'widget'   => 'single_text',
+                'format'   => 'yyyy-MM-dd',
+            ))
+            ->add('lignes', 'collection', array('type' => new BilletterieReservationLigneType()));
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
