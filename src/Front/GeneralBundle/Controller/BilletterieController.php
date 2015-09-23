c3254af5bbce9c048e43e7ae2d91a4e0cb6706fd
zied666 <zied.kharraz@gmail.com>
2015-09-23 14:03:16 +0100

    add 2 forms maritime & blletterie

--- src/Front/GeneralBundle/Controller/BilletterieController.php
+++ src/Front/GeneralBundle/Controller/BilletterieController.php
@@ -25,23 +25,27 @@ class BilletterieController extends Controller
         if (!is_null($client->getUser()))
             $reservation->setEmail($client->getUser()->getEmail());
         $form = $this->createForm(new BilletterieReservationType(), $reservation);
-        if($request->isMethod('POST'))
-        {
+        if ($request->isMethod('POST')) {
             $form->submit($request);
-            if(true)
-            {
-                $reservation=$form->getData();
+            if ($form->isValid()) {
+                $reservation = $form->getData();
                 $em->persist($reservation->setEtat(1)->setFrontOffice(1));
-                dump($reservation);
-                foreach($reservation->getLignes() as $ligne)
+                foreach ($reservation->getLignes() as $ligne)
                     $em->persist($ligne->setReservation($reservation));
                 $em->flush();
-                //return $this->redirect($this->generateUrl('front_billetterie'));
+                return $this->redirect($this->generateUrl('front_billetterie_succees',array('id'=>$reservation->getId())));
             }
         }
         return $this->render('FrontGeneralBundle:billetterie:reservation.html.twig', array(
                 'csrf_token' => $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate'),
                 'form'       => $form->createView())
         );
+    }
+
+    public function successAction(BilletterieReservation $reservation)
+    {
+        return $this->render("FrontGeneralBundle:billetterie:success.html.twig", array(
+            'reservation' => $reservation
+        ));
     }
 }
