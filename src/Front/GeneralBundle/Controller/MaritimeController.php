d95d1987be261e885e304b327049c43037c1c112
zied666 <zied.kharraz@gmail.com>
2015-09-30 14:18:59 +0100

    fix bug maritime token

--- src/Front/GeneralBundle/Controller/MaritimeController.php
+++ src/Front/GeneralBundle/Controller/MaritimeController.php
@@ -10,32 +10,33 @@ class MaritimeController extends Controller
 {
     public function reservationAction()
     {
-        $em=$this->getDoctrine()->getManager();
-        $request=$this->getRequest();
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
         $user = $this->get('security.context')->getToken()->getUser();
         if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') && !is_null($user->getClient()))
             $client = $user->getClient();
         else
             $client = $this->container->get('users')->getPassager();
-        $reservation= new MaritimeReservation();
+        $reservation = new MaritimeReservation();
         $reservation->setClient($client)
             ->setNomPrenom($client->getNomPrenom())
             ->setTel($client->getTel1());
         if (!is_null($client->getUser()))
             $reservation->setEmail($client->getUser()->getEmail());
-        $form=$this->createForm(new MaritimeReservationType(),$reservation);
-        if($request->isMethod('POST'))
-        {
+        $form = $this->createForm(new MaritimeReservationType(), $reservation);
+        if ($request->isMethod('POST')) {
             $form->submit($request);
-            if($form->isValid())
-            {
-                $reservation=$form->getData();
+            if ($form->isValid()) {
+                $reservation = $form->getData();
                 $em->persist($reservation->setEtat(1)->setFrontOffice(1));
                 $em->flush();
-                return $this->redirect($this->generateUrl('front_maritime_succees',array('id'=>$reservation->getId())));
+                return $this->redirect($this->generateUrl('front_maritime_succees', array('id' => $reservation->getId())));
             }
         }
-        return $this->render('FrontGeneralBundle:maritime:reservation.html.twig',array('form'=>$form->createView()));
+        return $this->render('FrontGeneralBundle:maritime:reservation.html.twig', array(
+                'csrf_token' => $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate'),
+                'form'       => $form->createView())
+        );
     }
 
     public function successAction(MaritimeReservation $reservation)
