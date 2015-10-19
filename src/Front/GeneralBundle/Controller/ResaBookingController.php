c1a5f9574ab904735350b2b962bfd7800797d5f9
zied666 <zied.kharraz@gmail.com>
2015-10-19 12:33:19 +0100

    add service room

--- src/Front/GeneralBundle/Controller/ResaBookingController.php
+++ src/Front/GeneralBundle/Controller/ResaBookingController.php
@@ -6,19 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ResaBookingController extends Controller
 {
-    public function indexAction()
+    public function listeAction($page,$region,$debut,$fin,$room1,$room2,$room3,$room4,$room5)
     {
         $em = $this->getDoctrine()->getManager();
-        $session = $this->getRequest()->getSession();
-        $request = $this->getRequest();
-        $sliders = $em->getRepository('FrontConfigBundle:SliderSHT')->findBy(array(), array('ordre' => 'asc'));
-        $pays = $em->getRepository('BackHotelTunisieBundle:Pays')->findOneBy(array('code' => 'tn'));
-        $villes = $em->getRepository('BackHotelTunisieBundle:Ville')->findBy(array('pays' => $pays), array('libelle' => 'asc'));
-//        if($request->isMethod('POST')) {
-//            $session->set('nuitees', $request->get('nuitees'));
-//            $session->set('dateDebut', $request->get('dateDebut'));
-//            return $this->redirect($this->generateUrl('front_hoteltunisie_list', array('ville' => $request->get('ville'), 'categorie' => $request->get('categorie'), 'name' => urlencode($request->get('motcles')),)));
-//        }
-        return $this->render('FrontGeneralBundle:resabooking:accueil.html.twig', array('villes' => $villes, 'sliders' => $sliders));
+        return $this->render('FrontGeneralBundle:resabooking/liste:liste.html.twig');
     }
 }
