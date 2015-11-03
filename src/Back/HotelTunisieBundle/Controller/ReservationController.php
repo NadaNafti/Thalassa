39bfe6fb7998d5349bc896fbcbc1a1c91ab11834
nada <nafti.nada@gmail.com>
2015-11-03 16:36:09 +0100

    add recues NADA

--- src/Back/HotelTunisieBundle/Controller/ReservationController.php
+++ src/Back/HotelTunisieBundle/Controller/ReservationController.php
@@ -341,8 +341,6 @@ class ReservationController extends Controller
 
     public function recusAction(Reservation $reservation)
     {
-        if ($reservation->getEtat() != 2)
-            return $this->redirect($this->generateUrl("liste_reservations"));
         $em = $this->getDoctrine()->getManager();
         return $this->render('BackHotelTunisieBundle:Reservation:recus.html.twig', array(
             'reservation' => $reservation,
