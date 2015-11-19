6f4e00a4683a3e62c138b2d6d644a8b5920876f2
nada <nafti.nada@gmail.com>
2015-11-19 09:10:01 +0100

    reporting chaffre affaire SHT

--- src/Back/HotelTunisieBundle/Controller/ReportingController.php
+++ src/Back/HotelTunisieBundle/Controller/ReportingController.php
@@ -74,4 +74,58 @@ class ReportingController extends Controller
             'dataSource'=>$dataSource,
         ));
     }
+    
+    public function chiffreAffaireAction()
+    {
+        $form = $this->createForm(new ReportingNombreReservationType());
+        $request=$this->getRequest();
+        if($request->isMethod('POST'))
+        {
+            $form->submit($request);
+            $data=$form->getData();
+            $array=array();
+            $array['annee']=$data['annee'];
+            $array['etat']=$data['etat'];
+            if(count($data['mois'])==0)
+                $array['mois']='all';
+            else
+            {
+                $mois=array();
+                foreach($data['mois'] as $moi)
+                    $mois[]=$moi;
+                $array['mois']=implode(',',$mois);
+            }
+            if(count($data['hotels'])==0)
+                $array['hotels']='all';
+            else
+            {
+                $hotels=array();
+                foreach($data['hotels'] as $hotel)
+                    $hotels[]=$hotel->getId();
+                $array['hotels']=implode(',',$hotels);
+            }
+            if(count($data['regions'])==0)
+                $array['regions']='all';
+            else
+            {
+                $regions=array();
+                foreach($data['regions'] as $region)
+                    $regions[]=$region->getId();
+                $array['regions']=implode(',',$regions);
+            }
+            if(count($data['users'])==0)
+                $array['users']='all';
+            else
+            {
+                $users=array();
+                foreach($data['users'] as $user)
+                    $users[]=$user->getId();
+                $array['users']=implode(',',$users);
+            }
+            return $this->redirect($this->generateUrl('Hotel_Tunisie_Reporting_ChiffreAffaire_stat',$array));
+        }
+        return $this->render('BackHotelTunisieBundle:Reporting:chiffre_affaire_form.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
